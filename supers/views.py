4e88624abb3c1b91d4895facebf57bd5425c374c
Judah0715 <judah.agnew121@gmail.com>
2022-11-01 11:28:36 -0400

    Registered Heroes and Villains Model with Admin Site + Completed POST Functiaonality for Heroes and Villains + Updated Rest Framework Decimal Settings

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,52 @@
-from django.db import models
+from django.shortcuts import get_object_or_404, render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SuperSerializer
+from .models import Super
+from supers.serializers import SuperSerializer
 
-# Create your models here.
+
+@api_view(['GET', 'POST']) 
+def supers_list(request):
+
+    supers = Super.objects.all()
+
+    if request.method == 'GET':
+
+        super_type_param = request.query_params.get('super_type') 
+        
+        if super_type_param == 'hero':
+            supers = supers.filter(super_type__id=1) 
+        elif super_type_param == 'villain':
+            supers = supers.filter(super_type__id=2)
+        else:
+            supers = Super.objects.all()
+        
+        serializer = SuperSerializer(supers, many = True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'POST':
+        serializer = SuperSerializer(data=request.data) 
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE']) 
+def super_detail(request,pk):
+
+    super = get_object_or_404(Super,pk=pk)
+
+    if request.method == 'GET':
+        serializer = SuperSerializer(super)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'PUT':
+        serializer = SuperSerializer(super, data=request.data) 
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'DELETE':
+        super.delete()
+        return Response(status.HTTP_204_NO_CONTENT) 
